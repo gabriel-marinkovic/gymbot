1200791c8ad5e670cc439f002965ce381e7ce10a
Gabriel Marinkovic <gabrielmarink@gmail.com>
2024-07-20 17:30:49 +0200

    inline keyboard example

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import sys
 import tomllib
 from typing import Any, Dict
 
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, filters, MessageHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, filters, MessageHandler, CallbackQueryHandler
 
 logging.basicConfig(
     encoding="utf-8",
@@ -29,27 +29,33 @@ def load_config() -> Dict[str, Any]:
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if not update.effective_chat:
-        logging.warn("Got update without 'effective_chat':", update)
+    if not update.message:
+        logging.warn("Got update without 'message':", update)
         return
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, talk to me!")
+
+    keyboard = [
+        [
+            InlineKeyboardButton("Option 1", callback_data="1"),
+            InlineKeyboardButton("Option 2", callback_data="2"),
+        ],
+        [InlineKeyboardButton("Option 3", callback_data="3")],
+    ]
+    await update.message.reply_text("Please choose:", reply_markup=InlineKeyboardMarkup(keyboard))
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if not update.effective_chat:
-        logging.warn("Got update without 'effective_chat':", update)
-        return
-    if update.message and update.message.text:
-        text = update.message.text
-    else:
-        logging.warn("Expected the update object to have 'message.text':", update)
-        text = "<DEFAULT TEXT>"
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+    assert query
+    # NOTE: CallbackQueries need to be answered, even if no notification to the
+    # user is needed Some clients may have trouble otherwise. See
+    # https://core.telegram.org/bots/api#callbackquery
+    await query.answer()
+    await query.edit_message_text(text=f"Selected option: {query.data}")
 
 
 if __name__ == "__main__":
     config = load_config()
     app = ApplicationBuilder().token(config["bot_auth_token"]).build()
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), echo))
+    app.add_handler(CallbackQueryHandler(button))
     app.run_polling(poll_interval=0.5)
