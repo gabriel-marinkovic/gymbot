6882acf0d6cb8f38c14709a9f9b63fa092d6f9a0
Gabriel Marinkovic <gabrielmarink@gmail.com>
2024-07-20 16:58:00 +0200

    echo

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import tomllib
 from typing import Any, Dict
 
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, filters, MessageHandler
 
 logging.basicConfig(
     encoding="utf-8",
@@ -35,8 +35,21 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, talk to me!")
 
 
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not update.effective_chat:
+        logging.warn("Got update without 'effective_chat':", update)
+        return
+    if update.message and update.message.text:
+        text = update.message.text
+    else:
+        logging.warn("Expected the update object to have 'message.text':", update)
+        text = "<DEFAULT TEXT>"
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+
+
 if __name__ == "__main__":
     config = load_config()
     app = ApplicationBuilder().token(config["bot_auth_token"]).build()
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), echo))
     app.run_polling(poll_interval=0.5)
