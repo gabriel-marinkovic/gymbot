e5dc367bc4dc83caba95eb572e474c65922f76b9
Gabriel Marinkovic <gabrielmarink@gmail.com>
2024-07-21 22:01:39 +0200

    exercise list

--- workouts.py
+++ workouts.py
@@ -5,6 +5,7 @@ from typing import List
 @dataclasses.dataclass
 class ExerciseTemplate:
     name: str
+    long_cycle_progression: bool
     weight: float
     weight_delta: float
     sets: int
@@ -13,18 +14,124 @@ class ExerciseTemplate:
 
 _default_exercise_templates = [
     ExerciseTemplate(
+        name="Squat",
+        long_cycle_progression=True,
+        weight=40.0,
+        weight_delta=5,
+        sets=3,
+        reps=12,
+    ),
+    ExerciseTemplate(
         name="Bench Press",
+        long_cycle_progression=True,
         weight=30.0,
         weight_delta=2.5,
         sets=3,
         reps=12,
     ),
     ExerciseTemplate(
-        name="Squat",
+        name="Barbell Row",
+        long_cycle_progression=True,
+        weight=30.0,
+        weight_delta=2.5,
+        sets=3,
+        reps=12,
+    ),
+    ExerciseTemplate(
+        name="Lying Tricep Extension",
+        long_cycle_progression=True,
+        weight=0,
+        weight_delta=2.5,
+        sets=3,
+        reps=12,
+    ),
+    ExerciseTemplate(
+        name="Leg Curl",
+        long_cycle_progression=True,
+        weight=0,
+        weight_delta=5,
+        sets=3,
+        reps=12,
+    ),
+    ExerciseTemplate(
+        name="Dumbbell Curl",
+        long_cycle_progression=True,
+        weight=6,
+        weight_delta=1,
+        sets=3,
+        reps=12,
+    ),
+    ExerciseTemplate(
+        name="Weighted Sit Up",
+        long_cycle_progression=False,
+        weight=25,
+        weight_delta=5,
+        sets=3,
+        reps=20,
+    ),
+    ExerciseTemplate(
+        name="Deadlift",
+        long_cycle_progression=True,
         weight=40.0,
         weight_delta=5,
         sets=3,
         reps=12,
+    ),
+    ExerciseTemplate(
+        name="Overhead Press",
+        long_cycle_progression=True,
+        weight=20,
+        weight_delta=2.5,
+        sets=3,
+        reps=12,
+    ),
+    ExerciseTemplate(
+        name="Pull Ups",
+        long_cycle_progression=False,
+        weight=0,
+        weight_delta=0,
+        sets=3,
+        reps=12,
+    ),
+    ExerciseTemplate(
+        name="Lat Pulls",
+        long_cycle_progression=False,
+        weight=25,
+        weight_delta=7,
+        sets=3,
+        reps=20,
+    ),
+    ExerciseTemplate(
+        name="Dips",
+        long_cycle_progression=False,
+        weight=0,
+        weight_delta=0,
+        sets=3,
+        reps=12,
+    ),
+    ExerciseTemplate(
+        name="Seated Calf Raise",
+        long_cycle_progression=True,
+        weight=20,
+        weight_delta=5,
+        sets=3,
+        reps=12,
+    ),
+    ExerciseTemplate(
+        name="Power Barbell Shrug",
+        long_cycle_progression=True,
+        weight=50,
+        weight_delta=2.5,
+        sets=3,
+        reps=12,
+    ),
+    ExerciseTemplate(
+        name="Plank",
+        long_cycle_progression=False,
+        weight=0,
+        weight_delta=0,
+        sets=3,
+        reps=60,
     ),
 ]
 
