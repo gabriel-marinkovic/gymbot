4573c5e624852b1f4ddc51085316ca6965ab6234
Gabriel Marinkovic <gabrielmarink@gmail.com>
2024-07-22 00:07:52 +0200

    wip, size limit hit

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import json
+import uuid
 import logging
 import os
 import sys
 import tomllib
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, List
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.constants import ParseMode
@@ -50,27 +52,76 @@ _workout = [
 ]
 
 
-def _generate_workout_markup(workout) -> InlineKeyboardMarkup:
-    keyboard = []
-    for exercise, sets in workout:
-        keyboard.append([InlineKeyboardButton(exercise.name, callback_data="bla")])
-        row = []
-        for i, s in enumerate(sets):
-            rep_label = f"{s.reps} ({exercise.weight}kg)"
-            row.append(InlineKeyboardButton(rep_label, callback_data=f"rep{i}"))
-        keyboard.append(row)
-        keyboard.append(
-            [
-                InlineKeyboardButton("⬆️ reps", callback_data="reps_up"),
-                InlineKeyboardButton("⬇️ reps", callback_data="reps_down"),
-                InlineKeyboardButton("⬆️ weight", callback_data="weight_up"),
-                InlineKeyboardButton("⬇️ weight", callback_data="weight_down"),
-            ]
-        )
-    return InlineKeyboardMarkup(keyboard)
+class InvalidWorkerIDError(ValueError):
+    pass
 
 
-active_workout: Optional[workouts.WorkoutT] = None
+class InvalidSetIDError(ValueError):
+    pass
+
+
+class UnknownKindError(ValueError):
+    pass
+
+
+class Workout:
+    id: str
+    exercises: List[workouts.Exercise]
+
+    def __init__(self, exercises: List[workouts.ExerciseTemplate]):
+        self.id = str(uuid.uuid4())
+        self.exercises = [workouts.Exercise(ex) for ex in exercises]
+
+    def generate_workout_markup(self) -> InlineKeyboardMarkup:
+        keyboard = []
+        for exercise in self.exercises:
+            keyboard.append([InlineKeyboardButton(exercise.template.name, callback_data="bla")])
+            row = []
+            for i, s in enumerate(exercise.sets):
+                checkbox = "✅ " if s.completed else ""
+                label = f"{checkbox}{s.reps} ({exercise.template.weight}kg)"
+                row.append(InlineKeyboardButton(label, callback_data=self._make_toggle_set_message(s)))
+            keyboard.append(row)
+            keyboard.append(
+                [
+                    InlineKeyboardButton("⬆️ reps", callback_data="reps_up"),
+                    InlineKeyboardButton("⬇️ reps", callback_data="reps_down"),
+                    InlineKeyboardButton("⬆️ weight", callback_data="weight_up"),
+                    InlineKeyboardButton("⬇️ weight", callback_data="weight_down"),
+                ]
+            )
+        return InlineKeyboardMarkup(keyboard)
+
+    def handle_message(self, message: Dict[str, Any]):
+        workout_id = message["workout_id"]
+        if workout_id != self.id:
+            raise InvalidWorkerIDError(workout_id)
+        kind = message["kind"]
+        if kind == Workout._MESSAGE_TOGGLE_SET:
+            set_id = message["set_id"]
+            toggled = False
+            for exercise in self.exercises:
+                for s in exercise.sets:
+                    if s.id == set_id:
+                        s.completed = not s.completed
+                        toggled = True
+            if not toggled:
+                raise InvalidSetIDError(set_id)
+        else:
+            raise UnknownKindError(kind)
+
+    _MESSAGE_TOGGLE_SET = "toggle_set"
+
+    def _make_toggle_set_message(self, s: workouts.WorkoutSet) -> str:
+        return self._make_message(Workout._MESSAGE_TOGGLE_SET, set_id=s.id)
+
+    def _make_message(self, kind: str, **kwargs) -> str:
+        x = json.dumps({"workout_id": self.id, "kind": kind, **kwargs})
+        print(x)
+        return x
+
+
+active_workout: Optional[Workout] = None
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -84,22 +135,35 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if active_workout:
         message += "<code>WARNING: Overwriting previous workout!</code>\n"
 
-    active_workout = workouts.make_workout()
+    active_workout = Workout(workouts.make_workout_template())
 
     await update.message.reply_text(
-        message,
-        parse_mode=ParseMode.HTML,
-        reply_markup=_generate_workout_markup(active_workout))
+        message, parse_mode=ParseMode.HTML, reply_markup=active_workout.generate_workout_markup()
+    )
 
 
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    global active_workout
+    assert active_workout
+
     query = update.callback_query
-    assert query
+    assert query and query.data
+    print(query.data)
+    assert isinstance(query.data, str)
+    try:
+        message: Dict[str, Any] = json.loads(query.data)
+    except ValueError as ex:
+        logging.error(f"Failed to decode callback_query: {ex}")
+    try:
+        active_workout.handle_message(message)
+    except ValueError as ex:
+        logging.error(f"Failed to handle message: {ex}")
+
     # NOTE: CallbackQueries need to be answered, even if no notification to the
     # user is needed Some clients may have trouble otherwise. See
     # https://core.telegram.org/bots/api#callbackquery
     await query.answer()
-    await query.edit_message_text(text=f"Selected option: {query.data}")
+    await query.edit_message_text("", reply_markup=active_workout.generate_workout_markup())
 
 
 if __name__ == "__main__":
--- workouts.py
+++ workouts.py
@@ -1,16 +1,10 @@
 import dataclasses
-from typing import List, Dict, Tuple
+from typing import List, Dict
+import uuid
 
 
 @dataclasses.dataclass
-class WorkoutSet:
-    weight: float
-    reps: int
-    completed: bool
-
-
-@dataclasses.dataclass
-class Exercise:
+class ExerciseTemplate:
     name: str
     long_cycle_progression: bool
     weight: float
@@ -19,16 +13,31 @@ class Exercise:
     reps: int
 
 
-    def generate_sets(self) -> List[WorkoutSet]:
-        return [
-            WorkoutSet(weight=self.weight, reps=self.reps, completed=False)
-            for i in range(self.sets)
+
+@dataclasses.dataclass
+class WorkoutSet:
+    id: str
+    weight: float
+    reps: int
+    completed: bool
+
+
+class Exercise:
+    id: str
+    template: ExerciseTemplate
+    sets: List[WorkoutSet]
+
+    def __init__(self, template: ExerciseTemplate):
+        self.id = str(uuid.uuid4())
+        self.template = template
+        self.sets = [
+            WorkoutSet(id=str(uuid.uuid4()), weight=template.weight, reps=template.reps, completed=False)
+            for i in range(template.sets)
         ]
 
-WorkoutT = List[Tuple[Exercise, List[WorkoutSet]]]
 
-_default_exercise_templates: List[Exercise] = [
-    Exercise(
+_default_exercise_templates: List[ExerciseTemplate] = [
+    ExerciseTemplate(
         name="Squat",
         long_cycle_progression=True,
         weight=40.0,
@@ -36,7 +45,7 @@ _default_exercise_templates: List[Exercise] = [
         sets=3,
         reps=12,
     ),
-    Exercise(
+    ExerciseTemplate(
         name="Bench Press",
         long_cycle_progression=True,
         weight=30.0,
@@ -44,7 +53,7 @@ _default_exercise_templates: List[Exercise] = [
         sets=3,
         reps=12,
     ),
-    Exercise(
+    ExerciseTemplate(
         name="Barbell Row",
         long_cycle_progression=True,
         weight=30.0,
@@ -52,7 +61,7 @@ _default_exercise_templates: List[Exercise] = [
         sets=3,
         reps=12,
     ),
-    Exercise(
+    ExerciseTemplate(
         name="Lying Tricep Extension",
         long_cycle_progression=True,
         weight=0,
@@ -60,7 +69,7 @@ _default_exercise_templates: List[Exercise] = [
         sets=3,
         reps=12,
     ),
-    Exercise(
+    ExerciseTemplate(
         name="Leg Curl",
         long_cycle_progression=True,
         weight=0,
@@ -68,7 +77,7 @@ _default_exercise_templates: List[Exercise] = [
         sets=3,
         reps=12,
     ),
-    Exercise(
+    ExerciseTemplate(
         name="Dumbbell Curl",
         long_cycle_progression=True,
         weight=6,
@@ -76,7 +85,7 @@ _default_exercise_templates: List[Exercise] = [
         sets=3,
         reps=12,
     ),
-    Exercise(
+    ExerciseTemplate(
         name="Weighted Sit Up",
         long_cycle_progression=False,
         weight=25,
@@ -84,7 +93,7 @@ _default_exercise_templates: List[Exercise] = [
         sets=3,
         reps=20,
     ),
-    Exercise(
+    ExerciseTemplate(
         name="Deadlift",
         long_cycle_progression=True,
         weight=40.0,
@@ -92,7 +101,7 @@ _default_exercise_templates: List[Exercise] = [
         sets=3,
         reps=12,
     ),
-    Exercise(
+    ExerciseTemplate(
         name="Overhead Press",
         long_cycle_progression=True,
         weight=20,
@@ -100,7 +109,7 @@ _default_exercise_templates: List[Exercise] = [
         sets=3,
         reps=12,
     ),
-    Exercise(
+    ExerciseTemplate(
         name="Pull Ups",
         long_cycle_progression=False,
         weight=0,
@@ -108,7 +117,7 @@ _default_exercise_templates: List[Exercise] = [
         sets=3,
         reps=12,
     ),
-    Exercise(
+    ExerciseTemplate(
         name="Lat Pulls",
         long_cycle_progression=False,
         weight=25,
@@ -116,7 +125,7 @@ _default_exercise_templates: List[Exercise] = [
         sets=3,
         reps=20,
     ),
-    Exercise(
+    ExerciseTemplate(
         name="Dips",
         long_cycle_progression=False,
         weight=0,
@@ -124,7 +133,7 @@ _default_exercise_templates: List[Exercise] = [
         sets=3,
         reps=12,
     ),
-    Exercise(
+    ExerciseTemplate(
         name="Seated Calf Raise",
         long_cycle_progression=True,
         weight=20,
@@ -132,7 +141,7 @@ _default_exercise_templates: List[Exercise] = [
         sets=3,
         reps=12,
     ),
-    Exercise(
+    ExerciseTemplate(
         name="Power Barbell Shrug",
         long_cycle_progression=True,
         weight=50,
@@ -140,7 +149,7 @@ _default_exercise_templates: List[Exercise] = [
         sets=3,
         reps=12,
     ),
-    Exercise(
+    ExerciseTemplate(
         name="Plank",
         long_cycle_progression=False,
         weight=0,
@@ -151,13 +160,10 @@ _default_exercise_templates: List[Exercise] = [
 ]
 
 
-_name_to_template: Dict[str, Exercise] = {
-    x.name: x
-    for x in _default_exercise_templates
-}
+_name_to_template: Dict[str, ExerciseTemplate] = {x.name: x for x in _default_exercise_templates}
 
 
-def make_workout() -> WorkoutT:
+def make_workout_template() -> List[ExerciseTemplate]:
     names = [
         "Squat",
         "Bench Press",
@@ -167,9 +173,4 @@ def make_workout() -> WorkoutT:
         "Dumbbell Curl",
         "Weighted Sit Up",
     ]
-    exercises = []
-    for name in names:
-        exercise = _name_to_template[name]
-        exercises.append((exercise, exercise.generate_sets()))
-    return exercises
-
+    return [_name_to_template[name] for name in names]
