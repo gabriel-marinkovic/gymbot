75bc7fba6b3af400d3ea1505d31ef189c4397168
Gabriel Marinkovic <gabrielmarink@gmail.com>
2024-07-22 22:14:11 +0200

    wip: actions

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import json
 import uuid
 import logging
 import os
 import sys
 import tomllib
-from typing import Any, Dict, Optional, List
+from typing import Any, Dict, Optional, List, Callable
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.constants import ParseMode
@@ -34,91 +33,75 @@ def load_config() -> Dict[str, Any]:
         exit(-1)
 
 
-_workout = [
-    {
-        "name": "Bench Press",
-        "sets": 3,
-        "reps": 12,
-        "weight": 40.0,
-        "weight_delta": 2.5,
-    },
-    {
-        "name": "Bench Press",
-        "sets": 3,
-        "reps": 12,
-        "weight": 40.0,
-        "weight_delta": 2.5,
-    },
-]
-
-
-class InvalidWorkerIDError(ValueError):
-    pass
-
-
-class InvalidSetIDError(ValueError):
-    pass
-
-
-class UnknownKindError(ValueError):
-    pass
-
-
 class Workout:
     id: str
     exercises: List[workouts.Exercise]
+    _actions: Dict[str, Callable[[], None]]
 
     def __init__(self, exercises: List[workouts.ExerciseTemplate]):
         self.id = str(uuid.uuid4())
         self.exercises = [workouts.Exercise(ex) for ex in exercises]
+        self._actions = {}
 
     def generate_workout_markup(self) -> InlineKeyboardMarkup:
         keyboard = []
         for exercise in self.exercises:
-            keyboard.append([InlineKeyboardButton(exercise.template.name, callback_data="bla")])
+            keyboard.append(
+                [InlineKeyboardButton(exercise.template.name, callback_data=self._register_action(lambda: None))]
+            )
             row = []
             for i, s in enumerate(exercise.sets):
                 checkbox = "✅ " if s.completed else ""
-                label = f"{checkbox}{s.reps} ({exercise.template.weight}kg)"
-                row.append(InlineKeyboardButton(label, callback_data=self._make_toggle_set_message(s)))
+                label = f"{checkbox}{s.reps} ({s.weight}kg)"
+                row.append(InlineKeyboardButton(label, callback_data=self._register_action_toggle_set_completed(s)))
             keyboard.append(row)
             keyboard.append(
                 [
-                    InlineKeyboardButton("⬆️ reps", callback_data="reps_up"),
-                    InlineKeyboardButton("⬇️ reps", callback_data="reps_down"),
-                    InlineKeyboardButton("⬆️ weight", callback_data="weight_up"),
-                    InlineKeyboardButton("⬇️ weight", callback_data="weight_down"),
+                    InlineKeyboardButton("⬆️ reps", callback_data=self._register_action_change_reps(exercise, True)),
+                    InlineKeyboardButton("⬇️ reps", callback_data=self._register_action_change_reps(exercise, False)),
+                    InlineKeyboardButton("⬆️ weight", callback_data=self._register_action_change_weight(exercise, True)),
+                    InlineKeyboardButton(
+                        "⬇️ weight", callback_data=self._register_action_change_weight(exercise, False)
+                    ),
                 ]
             )
         return InlineKeyboardMarkup(keyboard)
 
-    def handle_message(self, message: Dict[str, Any]):
-        workout_id = message["workout_id"]
-        if workout_id != self.id:
-            raise InvalidWorkerIDError(workout_id)
-        kind = message["kind"]
-        if kind == Workout._MESSAGE_TOGGLE_SET:
-            set_id = message["set_id"]
-            toggled = False
-            for exercise in self.exercises:
-                for s in exercise.sets:
-                    if s.id == set_id:
-                        s.completed = not s.completed
-                        toggled = True
-            if not toggled:
-                raise InvalidSetIDError(set_id)
-        else:
-            raise UnknownKindError(kind)
+    def run_action(self, action_id: str):
+        action = self._actions.get(action_id, None)
+        if action is None:
+            raise ValueError(f"Unknown action ID: {action_id}")
+        action()
 
-    _MESSAGE_TOGGLE_SET = "toggle_set"
+    def _register_action_toggle_set_completed(self, s: workouts.WorkoutSet) -> str:
+        def callback():
+            s.completed = not s.completed
 
-    def _make_toggle_set_message(self, s: workouts.WorkoutSet) -> str:
-        return self._make_message(Workout._MESSAGE_TOGGLE_SET, set_id=s.id)
+        return self._register_action(callback)
 
-    def _make_message(self, kind: str, **kwargs) -> str:
-        x = json.dumps({"workout_id": self.id, "kind": kind, **kwargs})
-        print(x)
-        return x
+    def _register_action_change_reps(self, exercise: workouts.Exercise, increase: bool) -> str:
+        def callback():
+            delta = 1 if increase else -1
+            for s in exercise.sets:
+                if not s.completed:
+                    s.reps = max(0, s.reps + delta)
+
+        return self._register_action(callback)
+
+    def _register_action_change_weight(self, exercise: workouts.Exercise, increase: bool) -> str:
+        def callback():
+            delta = exercise.template.weight_delta if increase else -exercise.template.weight_delta
+            for s in exercise.sets:
+                if not s.completed:
+                    s.weight = round(s.weight + delta, 2)
+
+        return self._register_action(callback)
+
+    def _register_action(self, callback: Callable[[], None]) -> str:
+        action_id = str(uuid.uuid4())
+        assert action_id not in self._actions
+        self._actions[action_id] = callback
+        return action_id
 
 
 active_workout: Optional[Workout] = None
@@ -148,22 +131,22 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     query = update.callback_query
     assert query and query.data
-    print(query.data)
-    assert isinstance(query.data, str)
-    try:
-        message: Dict[str, Any] = json.loads(query.data)
-    except ValueError as ex:
-        logging.error(f"Failed to decode callback_query: {ex}")
-    try:
-        active_workout.handle_message(message)
-    except ValueError as ex:
-        logging.error(f"Failed to handle message: {ex}")
+    action_id = query.data
+    assert isinstance(action_id, str)
 
     # NOTE: CallbackQueries need to be answered, even if no notification to the
     # user is needed Some clients may have trouble otherwise. See
     # https://core.telegram.org/bots/api#callbackquery
     await query.answer()
-    await query.edit_message_text("", reply_markup=active_workout.generate_workout_markup())
+
+    try:
+        active_workout.run_action(action_id)
+    except ValueError:
+        logging.warn(f"Action ID not present in actions list: {action_id}")
+        assert update.message
+        await update.message.reply_text("The desired action can't be processed. Please refresh the UI!")
+        return
+    await query.edit_message_reply_markup(active_workout.generate_workout_markup())
 
 
 if __name__ == "__main__":
